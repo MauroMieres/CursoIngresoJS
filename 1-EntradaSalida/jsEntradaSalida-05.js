374461bc982d7819c4506b5eb461f14cef4f54c3
MauroMieres <mauronicolasmieres@gmail.com>
2021-07-05 14:24:56 -0300

    Ejercicio 5 resuelto

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -3,6 +3,18 @@ Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
+	//declaro las dos variables que voy a utilizar
+	let nombre;
+	let edad;
+
+	// txtIdNombre ya esta declarado, es el nombre del placeholder en esa posicion
+	nombre = document.getElementById("txtIdNombre").value;
+
+	// txtIdEdad ya esta declarado, es el nombre del placeholder en esa posicion
+	edad = document.getElementById("txtIdEdad").value;
+
+	//muestro la informacion usando un alert y concateno entre datos y texto
+	alert("Usted se llama " + nombre + " y tiene " + edad + " años");
 }
 
+	
