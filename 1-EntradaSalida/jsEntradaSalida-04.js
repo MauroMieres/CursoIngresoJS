451f28efb53b3425de8c14de3a9d1c5c4d9cdd6c
MauroMieres <mauronicolasmieres@gmail.com>
2021-07-05 14:07:12 -0300

    Ejercicio 4 Resuelto

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,13 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	//reservo espacio en memoria para guardar el nombre del usuario
+	let nombre;
+
+	//guardo en la variable el texto que escribio en usuario dentro de la ventana de prompt
+	nombre = prompt("Ingrese su nombre");
+
+	//copio en nombre lo que tengo guardado en la variable nombre dentro de la caja de texto que se me muestra en la pagina
+	document.getElementById("txtIdNombre").value = nombre ;
 }
 
