044b3eace50ad342246bd3f9b6c4971b776998d1
MauroMieres <mauronicolasmieres@gmail.com>
2021-07-05 13:53:57 -0300

    Ejercicio 3 resuelto

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,9 +3,23 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	
+	let nombre; //declaro la variable "nombre"
+
+	nombre = document.getElementById("txtIdNombre").value;
+	//obtengo el valor de la variable usando la informacion -
+	//- almacenada en el documento con document.getElementById().value
+
+	alert(nombre);
+
+	//uso un alert para mostrar la informacion obtenida
+
+	document.getElementById("txtIdNombre").value = "";
+
+	//poniendo ""; logramos que luego de apretar el boton aceptar
+	//la pagina muestre denuevo el texto "Su nombre" en vez de 
+	//seguir mostrando el nombre que introducimos, con el valor
+	// "cadena vacia" = "";
 
 }
 
