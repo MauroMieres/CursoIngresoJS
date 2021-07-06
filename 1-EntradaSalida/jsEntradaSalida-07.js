0f6c5157672d672efbd3fd58ccd8ae01cb92ab8b
MauroMieres <mauronicolasmieres@gmail.com>
2021-07-06 09:51:38 -0300

    Ejercicio 7 resuelto

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,64 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	let num1;
+	let num2;
+	let resultado;
+
+	//parseInt(document.getElementById('txtIdNumeroUno').value); con esa linea primero
+	//obtengo el valor introducido en el documento y luego lo convierto a entero
+	num1 = parseInt(document.getElementById('txtIdNumeroUno').value);
+
+	num2 = parseInt(document.getElementById('txtIdNumeroDos').value);
+
+	resultado = num1 + num2;
+
+	alert("El resultado de la suma es: " + resultado)
 }
 
 function restar()
 {
-	
+	let num1;
+	let num2;
+	let resultado;
+
+	num1 = parseInt(document.getElementById('txtIdNumeroUno').value);
+
+	num2 = parseInt(document.getElementById('txtIdNumeroDos').value);
+
+	resultado = num1 - num2;
+
+	alert("El resultado de la resta es: " + resultado)
 }
 
 function multiplicar()
 { 
-	
+	let num1;
+	let num2;
+	let resultado;
+
+	num1 = parseInt(document.getElementById('txtIdNumeroUno').value);
+
+	num2 = parseInt(document.getElementById('txtIdNumeroDos').value);
+
+	resultado = num1 * num2;
+
+	alert("El resultado de la multiplicacion es: " + resultado)
 }
 
 function dividir()
 {
-	
+	let num1;
+	let num2;
+	let resultado;
+
+	//utilizo un parseFloat para no perder los decimales en ciertas divisiones
+	num1 = parseFloat(document.getElementById('txtIdNumeroUno').value);
+
+	num2 = parseFloat(document.getElementById('txtIdNumeroDos').value);
+
+	resultado = num1 / num2;
+
+	alert("El resultado de la division es: " + resultado)
 }
 
