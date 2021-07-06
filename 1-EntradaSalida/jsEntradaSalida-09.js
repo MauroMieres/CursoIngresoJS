7bad89f0a5036665da579b284653e14abdb423ca
MauroMieres <mauronicolasmieres@gmail.com>
2021-07-06 11:06:45 -0300

    Ejercicio 9 resuelto

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,18 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	let sueldo;
+	let aumento;
+	let sueldoNuevo;
 
-	alert("ok");
+	//utilizo un float porq necesito discrimar los enteros con decimales en este caso
+	sueldo = parseFloat(document.getElementById('txtIdSueldo').value);
+
+	aumento = sueldo * 0.10; //0.10 = 10%
+
+	sueldoNuevo = sueldo + aumento
+
+	//con esto muestro en la casilla "resultado" el resultado obtenido
+	document.getElementById('txtIdResultado').value = sueldoNuevo;
+
 }
